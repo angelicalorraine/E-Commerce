7622db8733259ed24ce05fd0f1c636fe69e4539f
Angelica Hernandez <gel.lorraine@gmail.com>
2021-06-15 01:50:17 -0700

    Updating models and routes relations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,11 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongstoMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+});
 
+Tag.belongstoMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+})
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
